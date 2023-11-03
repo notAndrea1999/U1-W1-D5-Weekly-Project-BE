45a0f715257abef70e49d5a98dd9d0e3f9f7c0a9
Andrea De Masi <andreademasi1999@gmail.com>
2023-11-03 14:09:54 +0100

    Initial commit

---
+++ src/main/java/andreademasi/U1W1D5WeeklyProjectApplication.java
@@ -0,0 +1,13 @@
+package andreademasi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U1W1D5WeeklyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U1W1D5WeeklyProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/andreademasi/U1W1D5WeeklyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package andreademasi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U1W1D5WeeklyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
